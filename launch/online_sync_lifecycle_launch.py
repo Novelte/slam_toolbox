b2a94c0145b6060bf86738e9fe884af9b162f248
royCheung-223 <cwhvhhh@gmail.com>
2022-07-11 01:52:52 +0000

    update config file path

--- launch/online_sync_lifecycle_launch.py
+++ launch/online_sync_lifecycle_launch.py
@@ -14,8 +14,7 @@ def generate_launch_description():
 
     declare_slam_params_file_cmd = DeclareLaunchArgument(
         'slam_params_file',
-        default_value=os.path.join(get_package_share_directory("slam_toolbox"),
-                                   'config', 'mapper_online_sync_lifecycle.yaml'),
+        default_value= '/var/novelte/config/nav_config.yaml',
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
     start_sync_slam_toolbox_node = LifecycleNode(
